c25800d2e5e822205cee389b0507a1be8a1e1ee9
c5sire <rsimon64@gmail.com>
2018-09-21 10:53:15 -0500

    matrix calculation

--- swa.R
+++ swa.R
@@ -13,7 +13,6 @@ smx <- matrix(0, nrow = n_r,  ncol = n_c)
 sqv1 <- c(" ", string2vec(seq1))
 sqv2 <- c(" ", string2vec(seq2))
 
-
 gap_penalty <- -2
 ltr_match <- 2
 ltr_mismt <- -1
@@ -21,4 +20,15 @@ ltr_mismt <- -1
 colnames(smx) <- sqv2
 row.names(smx) <- sqv1
 
+# calculate  scores
+for (ri in 2:n_r) {
+  for (ci in 2:n_c) {
+    sf <- ifelse(sqv1[ri] == sqv2[ci], ltr_match, ltr_mismt)
+    s_s <- smx[ri - 1, ci - 1] + sf
+    s_r <- smx[ri - 1, ci] + gap_penalty
+    s_c <- smx[ri, ci - 1] + gap_penalty
+    s_a <- max(s_s, s_r, s_c, 0)
+    smx[ri, ci] <- s_a
+  }
+}
 
